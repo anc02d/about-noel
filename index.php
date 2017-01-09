fa007bebeb5e3d1c131822b721160f52674e519f
Noel Cothren <noelcothren@gmail.com>
2017-01-09 16:18:35 -0700

    Fixed my css file link in the index file centered the background and other image

--- index.php
+++ index.php
@@ -2,13 +2,10 @@
 <html>
 	<head>
 		<title>About Noel</title>
-		<style>
-
-		</style>
+		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
 		<h1>Noel Cothren</h1>
-		<img src="images/woodBkgd.jpg" alt="Wood Texture" align="center">
 		<img src="images/artSkeleton.jpg" alt="A Picture of Noel."
 			  style="width: 300px;height:auto" align="center">
 		<p>This is about me, Noel.</p>
