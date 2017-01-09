eae5f164884155382423de877c2b4e25cf4dba49
Noel Cothren <noelcothren@gmail.com>
2017-01-09 16:45:45 -0700

    edited content of first paragraph

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 		<h1 align="center">Noel Cothren</h1>
 		<img src="images/artSkeleton.jpg" alt="A Picture of Noel."
 			  style="width: 300px;height:auto" align="center">
-		<p>This is about me, Noel. I am currently learning Web Development at the CNM Stemulus Center's Deep Dive Coding Bootcamp.
-		Before coming to Deep Dive, I was an Office and HR Manager at Mario's Pizza here in Albuquerque, and before that I taught English to engineering students
+		<p>This site is about me, Noel. I am currently learning Web Development at the CNM Stemulus Center's Deep Dive Coding Bootcamp.
+		Before coming to Deep Dive, I was an Office and HR Manager at Mario's Pizza here in Albuquerque. Before that I taught English to engineering students
 		in Bogota, Colombia. I've also formally studied Fine Arts and Language Education, and have a strong base and continuing interest in psychology and biology.</p>
 		<p>In my free time, I read both non-fiction and fiction extensively. I also love to make things and frequently can be found doing arts and craft projects,
 			most recently focused in printmaking, pop-up cards, and woodworking. I live in Albuquerque, NM with my partner, Robert, and more than one billion tiny pets
