51b878f0a0f356564e8601fa32eb14fd5d4d780b
Noel Cothren <noelcothren@gmail.com>
2017-01-10 07:01:38 -0700

    added two images

--- index.php
+++ index.php
@@ -2,15 +2,22 @@
 <html>
 	<head>
 		<title>About Noel</title>
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"/>
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
-		<h1 align="center">Noel Cothren</h1>
+		<h1 align="center">Noel's Little Website</h1>
+		<h2 align="center">all about me!</h2>
 		<img src="images/artSkeleton.jpg" alt="A Picture of Noel."
 			  style="width: 300px;height:auto" align="center">
-		<p>This site is about me, Noel. I am currently learning Web Development at the CNM Stemulus Center's Deep Dive Coding Bootcamp.
-		Before coming to Deep Dive, I was an Office and HR Manager at Mario's Pizza here in Albuquerque. Before that I taught English to engineering students
-		in Bogota, Colombia. I've also formally studied Fine Arts and Language Education, and have a strong base and continuing interest in psychology and biology.</p>
+		<div class="row">
+			<div class="col-md-8"><p>This site is about me, Noel. I am currently learning Web Development at the CNM Stemulus Center's Deep Dive Coding Bootcamp.
+					Before coming to Deep Dive, I was an Office and HR Manager at Mario's Pizza here in Albuquerque. Before <em>that</em>, I taught English to engineering students
+				in Bogota, Colombia. I've also formally studied Fine Arts and Language Education, and have a strong base and continuing interest in psychology and biology.
+			</p></div>
+			<div class="col-md-4"> <img src="/images/unal.jpg" class="img-rounded" alt="Bogota" width="auto" height="auto">
+			</div>
+		</div>
 		<p>In my free time, I read both non-fiction and fiction extensively. I also love to make things and frequently can be found doing arts and craft projects,
 			most recently focused in printmaking, pop-up cards, and woodworking. I live in Albuquerque, NM with my partner, Robert, and more than one billion tiny pets
 			in our indoor garden, coral reef tank, and three freshwater fish tanks.</p>
