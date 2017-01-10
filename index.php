ba8f90ef5d729f05952dc621730a63218288a068
Noel Cothren <noelcothren@gmail.com>
2017-01-10 07:07:03 -0700

    changed colomn widths

--- index.php
+++ index.php
@@ -11,15 +11,22 @@
 		<img src="images/artSkeleton.jpg" alt="A Picture of Noel."
 			  style="width: 300px;height:auto" align="center">
 		<div class="row">
-			<div class="col-md-8"><p>This site is about me, Noel. I am currently learning Web Development at the CNM Stemulus Center's Deep Dive Coding Bootcamp.
+			<div class="col-md-7"><p>This site is about me, Noel. I am currently learning Web Development at the CNM Stemulus Center's Deep Dive Coding Bootcamp.
 					Before coming to Deep Dive, I was an Office and HR Manager at Mario's Pizza here in Albuquerque. Before <em>that</em>, I taught English to engineering students
-				in Bogota, Colombia. I've also formally studied Fine Arts and Language Education, and have a strong base and continuing interest in psychology and biology.
+				at the National University in Bogota, Colombia. I've also formally studied Fine Arts and Language Education, and have a strong base and continuing interest in psychology.
 			</p></div>
-			<div class="col-md-4"> <img src="/images/unal.jpg" class="img-rounded" alt="Bogota" width="auto" height="auto">
+			<div class="col-md-5"> <img src="images/unal.jpg" class="img-rounded" alt="Universidad Nacional" width="auto" height="auto">
 			</div>
 		</div>
-		<p>In my free time, I read both non-fiction and fiction extensively. I also love to make things and frequently can be found doing arts and craft projects,
+		<div class="row">
+			<div class="col-md-5"> <img src="images/truckcook.jpg" class="img-rounded" alt="Cooking on the Truck" width="auto" height="auto">
+			</div>
+		<div class="col-md-7">
+			<p>In my free time, I read both non-fiction and fiction extensively. I also love to make things and frequently can be found doing arts and craft projects,
 			most recently focused in printmaking, pop-up cards, and woodworking. I live in Albuquerque, NM with my partner, Robert, and more than one billion tiny pets
-			in our indoor garden, coral reef tank, and three freshwater fish tanks.</p>
+			in our indoor garden, coral reef tank, and three freshwater fish tanks.
+			</p>
+		</div>
+		</div>
 	</body>
 </html>
