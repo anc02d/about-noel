0723a480e896070dbcb0030dfd2c098b69d86fa9
Noel Cothren <noelcothren@gmail.com>
2017-01-14 12:07:24 -0700

    added to vendor and node modules to gitignore

--- index.php
+++ index.php
@@ -8,32 +8,26 @@
 	<body>
 		<h1 align="center">Noel's Little Website</h1>
 		<h2 align="center">all about me!</h2>
-		<img src="images/artSkeleton.jpg" alt="A Picture of Noel."
-			  style="width: 300px;height:auto" align="center">
+		<img class="me" src="images/artSkeleton.jpg" alt="A Picture of Noel." align="center">
 		<div class="row">
-			<div class="col-md-7"><p>This site is about me, Noel. I am currently learning Web Development at the CNM Stemulus Center's Deep Dive Coding Bootcamp.
-					Before coming to Deep Dive, I was an Office and HR Manager at Mario's Pizza here in Albuquerque. Before <em>that</em>, I taught English to engineering students
-				at the National University in Bogota, Colombia, and for one year I lived in a semi-truck with my partner,
-					Robert while he worked as an over-the-road truck driver. I've also formally studied Fine Arts and Language Education, and have a strong base and continuing interest in psychology.
+			<div class="col-md-7"><p>This site is about me, Noel. I am currently learning Web Development at the CNM Stemulus Center's Deep Dive Coding Bootcamp. Before coming to Deep Dive, I was an Office and HR Manager at Mario's Pizza here in Albuquerque. <em>Before that</em>, I taught English to engineering students at the National University in Bogota, Colombia, and for one year I lived in a semi-truck with my partner, Robert while he worked as an over-the-road truck driver. I've also formally studied Fine Arts and Language Education, and have a strong base and continuing interest in psychology.
 			</p></div>
-			<div class="col-md-5"> <img src="images/unal.jpg" class="img-rounded" alt="Universidad Nacional" width="auto" height="auto">
+			<div class="col-md-5"> <img src="images/unal.jpg" class="img-rounded" alt="Universidad Nacional" width="300" height="auto">
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-md-5"> <img src="images/truckcook.jpg" class="img-rounded" alt="Cooking on the Truck" width="auto" height="auto">
+			<div class="col-md-5"> <img src="images/truckcook.jpg" class="img-rounded" alt="Cooking on the Truck" width="300" height="auto">
 				</div>
 		<div class="col-md-7">
-			<p>In my free time, I read both non-fiction and fiction extensively. I also love to make things and frequently can be found doing arts and craft projects,
-			most recently focused in printmaking, pop-up cards, and woodworking. I live in Albuquerque, NM with my partner, Robert, and more than one billion tiny pets
-			in our indoor garden, coral reef tank, and three freshwater fish tanks.
+			<p>In my free time, I read both non-fiction and fiction extensively. I also love to make things and frequently can be found doing arts and craft projects, most recently focused in printmaking, pop-up cards, and woodworking. I live in Albuquerque, NM with my partner, Robert, and more than one billion tiny pets in our indoor garden, coral reef tank, and three freshwater fish tanks.
 			</p>
 		</div>
 		</div>
 		<footer>
 			<div class="row">
-				<div class="col-md-4"><p>Contact Me!</p>
-				<p>noelcothren@gmail.com</p>
-				<p>instagram.com/noeynoeinoee</p></div>
+				<div class="col-md-4"><p>Contact Me!<br>
+				noelcothren@gmail.com<br>
+				instagram.com/noeynoeinoee</p></div>
 			</div>
 		</footer>
 	</body>
