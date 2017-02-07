0e391cfd5d7b17ee4a5e09fd8f259bc251e170b1
Noel Cothren <noelcothren@gmail.com>
2017-02-07 16:27:24 -0700

    playing with the style

--- index.php
+++ index.php
@@ -1,31 +1,33 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>About Noel</title>
 		<link rel="stylesheet" href="style.css" type="text/css">
+		<title>About Noel</title>
 	</head>
+
 	<body>
 		<h1>Noel's Little Website</h1>
 		<h2>all about me!</h2>
+
 			<div>
 				<p>This site is about me, Noel. I am currently learning Web Development at the CNM Stemulus Center's Deep Dive Coding Bootcamp. Before coming to Deep Dive, I was an Office and HR Manager at Mario's Pizza here in Albuquerque. <em>Before that</em>, I taught English to engineering students at the National University in Bogota, Colombia, and for one year I lived in a semi-truck with my partner, Robert while he worked as an over-the-road truck driver. I've also formally studied Fine Arts and Language Education, and have a strong base and continuing interest in psychology.
 			</p>
 			</div>
-			<div> <img src="images/unal.jpg" alt="Universidad Nacional" width="300" height="auto">
-			</div>
-			<div> <img src="images/truckcook.jpg" alt="Cooking on the Truck" width="300" height="auto">
-				</div>
-		<div>
+
+ 				<img src="images/unal.jpg" alt="Universidad Nacional" width="300" height="auto">
+ 				<img src="images/truckcook.jpg" alt="Cooking on the Truck" width="300" height="auto">
+
+			<div>
 			<p>In my free time, I read both non-fiction and fiction extensively. I also love to make things and frequently can be found doing arts and craft projects, most recently focused in printmaking, pop-up cards, and woodworking. I live in Albuquerque, NM with my partner, Robert, and more than one billion tiny pets in our indoor garden, coral reef tank, and three freshwater fish tanks.
 			</p>
-		</div>
-		</>
+			</div>
+
 		<footer>
-			<div>
 				<div><p>Contact Me!<br>
 				noelcothren@gmail.com<br>
 				instagram.com/noeynoeinoee</p></div>
-			</div>
 		</footer>
+
 	</body>
+
 </html>
